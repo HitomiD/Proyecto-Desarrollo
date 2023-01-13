7a0dea77e0fd47810908941ef8d6d3773d7ca46d
Hitomi <hitomidiaconchuk@gmail.com>
2023-01-13 01:35:47 -0300

    Creacion (desde main.py) y poblado manual de base de datos.

--- windows/main.py
+++ windows/main.py
@@ -1,6 +1,34 @@
+import sqlite3
 from PySide6.QtWidgets import QApplication
 import sys
 from ventanas import *         
+
+dbconnection = sqlite3.connect("database.db")
+try:
+    dbconnection.execute("""
+                        CREATE TABLE Proveedores (
+                            CUIL_CUIT INTEGER PRIMARY KEY NOT NULL,
+                            razonsocial TEXT NOT NULL,
+                            direccion TEXT,
+                            telefono INTEGER,
+                            email TEXT
+                        );
+                        """)
+    
+    dbconnection.execute("""
+                        CREATE TABLE Productos (
+                            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                            descripcion TEXT NOT NULL,
+                            stock INTEGER NOT NULL DEFAULT '0',
+                            stock_minimo INTEGER,
+                            precio_venta FLOAT,
+                            CUIL_CUIT_proveedor INTEGER NOT NULL,
+                            FOREIGN KEY(CUIL_CUIT_proveedor) REFERENCES Proveedores(CUIL_CUIT)
+                        );
+                        """)
+except Exception as err:
+        print(str(err))
+dbconnection.close()
 
 def run():
     app = QApplication([])
